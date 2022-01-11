e7e055903ff666b25b6cb06afe0d0c6fceee328b
onodera_sf <sf_yo_service_yellow@outlook.jp>
2022-01-11 11:36:38 +0900

    プロジェクト ファイルを追加します。

---
+++ WinFormsDotNetFramework48/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("WinFormsDotNetFramework48")]
+[assembly: AssemblyDescription("選択しているビルド構成によって実行ファイルのアイコンを変更するサンプルです。")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ソーサリーフォース (https://sorceryforce.net/)")]
+[assembly: AssemblyProduct("WinFormsDotNetFramework48")]
+[assembly: AssemblyCopyright("Copyright (C) 2022 おのでら")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("98e7c0cb-7022-45d6-9fba-b196f96ce2be")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
